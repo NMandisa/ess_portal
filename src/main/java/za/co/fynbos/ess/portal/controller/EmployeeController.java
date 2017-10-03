fed526b8872d68f5a67630763049145d48734b94
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-03 12:59:31 +0200

    Initial Commit

--- src/main/java/za/co/fynbos/ess/portal/controller/EmployeeController.java
+++ src/main/java/za/co/fynbos/ess/portal/controller/EmployeeController.java
@@ -1,9 +1,21 @@
 package za.co.fynbos.ess.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.co.fynbos.ess.portal.service.EmployeeService;
+
 /**
  * @author NMandisa Mkhungo
  *
  */
+
+@CrossOrigin(allowedHeaders = {"*" }, origins = "*")
+@RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
 
+	@Autowired
+	EmployeeService employeeService;
+	
+	
 }
