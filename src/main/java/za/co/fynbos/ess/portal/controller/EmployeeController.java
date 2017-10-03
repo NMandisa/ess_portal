14a7c4b7b011d94d888ba4f818844ab8d5d82797
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-03 13:05:47 +0200

    Initail Commit

--- src/main/java/za/co/fynbos/ess/portal/controller/EmployeeController.java
+++ src/main/java/za/co/fynbos/ess/portal/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package za.co.fynbos.ess.portal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import za.co.fynbos.ess.portal.domain.Employee;
 import za.co.fynbos.ess.portal.service.EmployeeService;
 
 /**
@@ -17,5 +21,9 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployee();
+    }
 	
-}
+} 
--- src/main/java/za/co/fynbos/ess/portal/service/EmployeeService.java
+++ src/main/java/za/co/fynbos/ess/portal/service/EmployeeService.java
@@ -1,9 +1,14 @@
 package za.co.fynbos.ess.portal.service;
 
+import java.util.List;
+
+import za.co.fynbos.ess.portal.domain.Employee;
+
 /**
  * @author NMandisa Mkhungo
  *
  */
 public interface EmployeeService {
 
+    List<Employee> getAllEmployee();
 }
