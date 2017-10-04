e8184031d9c0648e47c902f2cb926cf0df3fa4b5
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-04 16:28:20 +0200

    Change-Id: I88a17765871e49be85518ccba10b3d136db6639f

--- src/main/java/za/co/fynbos/ess/portal/domain/Employee.java
+++ src/main/java/za/co/fynbos/ess/portal/domain/Employee.java
@@ -19,47 +19,63 @@ public class Employee implements Serializable {
 	private Long employeeId;
 	private String name;
 	private String surname;
-	private Date dob;
+	private String dob;
 	private String employeeNum;
 
+	public Employee() {}
+	public Employee(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public Employee(Long employeeId,String name) {
+		this.employeeId = employeeId;
+		this.name = name;
+	}
+	public Employee(Long employeeId,String name,String surname) {
+		this.employeeId = employeeId;
+		this.name = name;
+		this.surname = surname;
+	}
+	public Employee(Long employeeId,String name,String surname,String dob) {
+		this.employeeId = employeeId;
+		this.name = name;
+		this.surname = surname;
+		this.dob = dob;
+	}
+	public Employee(Long employeeId,String name,String surname,String dob,String employeeNum) {
+		this.employeeId = employeeId;
+		this.name = name;
+		this.surname = surname;
+		this.dob = dob;
+		this.employeeNum = employeeNum;
+	}
 	public Long getEmployeeId() {
 		return employeeId;
 	}
-
 	public void setEmployeeId(Long employeeId) {
 		this.employeeId = employeeId;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getSurname() {
 		return surname;
 	}
-
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-
-	public Date getDob() {
+	public String getDob() {
 		return dob;
 	}
-
-	public void setDob(Date dob) {
+	public void setDob(String dob) {
 		this.dob = dob;
 	}
-
 	public String getEmployeeNum() {
 		return employeeNum;
 	}
-
 	public void setEmployeeNum(String employeeNum) {
 		this.employeeNum = employeeNum;
 	}
-
 }
--- src/test/java/za/co/fynbos/ess/portal/repository/EmployeeRepositoryTest.java
+++ src/test/java/za/co/fynbos/ess/portal/repository/EmployeeRepositoryTest.java
@@ -3,9 +3,22 @@
  */
 package za.co.fynbos.ess.portal.repository;
 
+import static org.junit.Assert.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.poi.ss.util.DateFormatConverter;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ch.qos.logback.classic.pattern.DateConverter;
+import za.co.fynbos.ess.portal.domain.Employee;
 
 /**
  * @author NMandisa Mkhungo
@@ -17,5 +30,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DataJpaTest
 public class EmployeeRepositoryTest {
 	
+	@Autowired
+    private TestEntityManager entityManager;
+	
+	@Autowired
+    private EmployeeRepository employeeRepository;
+	
+	@Test
+    public void testFindByName() {
+        entityManager.persist(new Employee((long) 1,"Noxolo Mandisa","Mkhungo","30/04/1993","12192"));
+        List<Employee> employee = employeeRepository.findByName("Foo");
+        assertEquals("Foo", employee.get(0).getName());
+    }
 	
 }
