130e0199df5c7e039fc9777cc8cbb8bd520956c4
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-05 12:35:21 +0200

    Change-Id: I357a1db2e479998497365b646ece883f4762927c

--- src/main/java/za/co/fynbos/ess/portal/EssPortalApplication.java
+++ src/main/java/za/co/fynbos/ess/portal/EssPortalApplication.java
@@ -3,8 +3,6 @@ package za.co.fynbos.ess.portal;
 import java.io.Console;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,15 +25,15 @@ public class EssPortalApplication {
 		// System.out.println("*******************************************************");
 		EmployeeExcelRead employeeExcelRead = new EmployeeExcelRead();
 		List<Employee> employees = employeeExcelRead.EmployeeExcelRead(System.getProperty("user.dir")+"/src/main/resources/sample_data.xlsx");
-		Iterator it = employees.iterator();
-		Employee tempEmployee = new Employee();
-		  while(it.hasNext())
-		  {
-		    tempEmployee = (Employee) it.next();
+		//Iterator it = employees.iterator();
+		//Employee tempEmployee = new Employee();
+		  //while(it.hasNext())
+		//  {
+		    // tempEmployee = (Employee) it.next();
 		  
 		    //System.out.println("*******************************************************");
 		    //System.out.println("| You Are Here |");
 		    //System.out.println("*******************************************************");
-		  }
+		  //}
 	}
 }
--- src/main/java/za/co/fynbos/ess/portal/excel/read/EmployeeExcelRead.java
+++ src/main/java/za/co/fynbos/ess/portal/excel/read/EmployeeExcelRead.java
@@ -28,68 +28,43 @@ public class EmployeeExcelRead {
 	
 	public List<Employee> EmployeeExcelRead(String filename) {
 	
-    try {
+		try {
 
-        FileInputStream excelFile = new FileInputStream(new File(filename));
-        Workbook workbook = new XSSFWorkbook(excelFile);
+            FileInputStream excelFile = new FileInputStream(new File(filename));
+            Workbook workbook = new XSSFWorkbook(excelFile);
 
-        Sheet employeeDataSheet = workbook.getSheetAt(0);
-        List<Employee> employees = new ArrayList<>();//Initailizing a list of employees
-        Iterator<Row> iterator = employeeDataSheet.iterator();
+            Sheet employeeDataSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = employeeDataSheet.iterator();
 
-        while (iterator.hasNext()) {
+            while (iterator.hasNext()) {
 
-            Row currentRow = iterator.next();
-            Iterator<Cell> cellIterator = currentRow.iterator();
-            
-            Employee tempEmployee = new Employee();//Initializing and Employee Object everytime it loops through        
-            
-            while (cellIterator.hasNext()) {
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
 
-                Cell currentCell = cellIterator.next();
-                int columnIndex = currentCell.getColumnIndex();
-                
-                switch (columnIndex) {
-                case 0 : 
-                	tempEmployee.setName((String)currentCell.getStringCellValue());
-                break;
-                case 1 :
-                	tempEmployee.setSurname((String)currentCell.getStringCellValue());
-                
-                break;
-                case 2 :
-                	tempEmployee.setDob((String)currentCell.getStringCellValue());
-                break;
-                case 3 :
-                	System.out.println("*******************************************************");
-            		System.out.println("|___________________| You Are Here |___________________|");
-            		System.out.println("*******************************************************");
-                	tempEmployee.setEmployeeNum((String)currentCell.getStringCellValue());
-                break;
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + " | ");
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + " | ");
+                    }
+
                 }
-                if(!"Name".equals(tempEmployee.getName())) 
-                   employees.add(tempEmployee);
-                
-                
-                
-                //if (currentCell.getCellTypeEnum() == CellType.STRING) {
-                 //   System.out.print(currentCell.getStringCellValue() + " | ");
-               // } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-                  //  System.out.print(currentCell.getNumericCellValue() + " | ");
-                //}
+                System.out.println();
+                return null;
+            }
+        } catch (FileNotFoundException exception) {
+        	//return null;
+            exception.printStackTrace();
+        } catch (IOException exception) {
+        	//return null;
+            exception.printStackTrace();
+        }
+		return null;
 
-            
-            System.out.println();
-            return employees;
-        }}    } catch (FileNotFoundException exception) {
-        exception.printStackTrace();
-        return null;
-    } catch (IOException exception) {
-        exception.printStackTrace();
-        return null;
     }
-	return null;
 
-}
+
 
 }
