6504de39d4d313b4004b08be36d0c208be1c8640
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-05 12:00:35 +0200

    Change-Id: Iedd2529994dc98b1bd6ff37ef21c14fd3d6b0b5c

--- src/main/java/za/co/fynbos/ess/portal/EssPortalApplication.java
+++ src/main/java/za/co/fynbos/ess/portal/EssPortalApplication.java
@@ -1,13 +1,41 @@
 package za.co.fynbos.ess.portal;
 
+import java.io.Console;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import za.co.fynbos.ess.portal.domain.Employee;
+import za.co.fynbos.ess.portal.excel.read.EmployeeExcelRead;
 
 
 @SpringBootApplication
 public class EssPortalApplication {
-
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EssPortalApplication.class, args);
+		
+		 //System.out.println("*******************************************************");
+		// System.out.println("|___________________| You Are Here |___________________|");
+		// System.out.println("*******************************************************");
+		EmployeeExcelRead employeeExcelRead = new EmployeeExcelRead();
+		List<Employee> employees = employeeExcelRead.EmployeeExcelRead(System.getProperty("user.dir")+"/src/main/resources/sample_data.xlsx");
+		Iterator it = employees.iterator();
+		Employee tempEmployee = new Employee();
+		  while(it.hasNext())
+		  {
+		    tempEmployee = (Employee) it.next();
+		  
+		    //System.out.println("*******************************************************");
+		    //System.out.println("| You Are Here |");
+		    //System.out.println("*******************************************************");
+		  }
 	}
 }
--- src/main/java/za/co/fynbos/ess/portal/excel/read/EmployeeExcelRead.java
+++ src/main/java/za/co/fynbos/ess/portal/excel/read/EmployeeExcelRead.java
@@ -7,7 +7,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -16,13 +18,15 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import za.co.fynbos.ess.portal.domain.Employee;
+
 /**
  * @author NMandisa Mkhungo
  *
  */
 public class EmployeeExcelRead {
 	
-	public EmployeeExcelRead(String filename) {
+	public List<Employee> EmployeeExcelRead(String filename) {
 	
     try {
 
@@ -30,31 +34,61 @@ public class EmployeeExcelRead {
         Workbook workbook = new XSSFWorkbook(excelFile);
 
         Sheet employeeDataSheet = workbook.getSheetAt(0);
+        List<Employee> employees = new ArrayList<>();//Initailizing a list of employees
         Iterator<Row> iterator = employeeDataSheet.iterator();
 
         while (iterator.hasNext()) {
 
             Row currentRow = iterator.next();
             Iterator<Cell> cellIterator = currentRow.iterator();
-
+            
+            Employee tempEmployee = new Employee();//Initializing and Employee Object everytime it loops through        
+            
             while (cellIterator.hasNext()) {
 
                 Cell currentCell = cellIterator.next();
-                if (currentCell.getCellTypeEnum() == CellType.STRING) {
-                    System.out.print(currentCell.getStringCellValue() + " | ");
-                } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-                    System.out.print(currentCell.getNumericCellValue() + " | ");
+                int columnIndex = currentCell.getColumnIndex();
+                
+                switch (columnIndex) {
+                case 0 : 
+                	tempEmployee.setName((String)currentCell.getStringCellValue());
+                break;
+                case 1 :
+                	tempEmployee.setSurname((String)currentCell.getStringCellValue());
+                
+                break;
+                case 2 :
+                	tempEmployee.setDob((String)currentCell.getStringCellValue());
+                break;
+                case 3 :
+                	System.out.println("*******************************************************");
+            		System.out.println("|___________________| You Are Here |___________________|");
+            		System.out.println("*******************************************************");
+                	tempEmployee.setEmployeeNum((String)currentCell.getStringCellValue());
+                break;
                 }
+                if(!"Name".equals(tempEmployee.getName())) 
+                   employees.add(tempEmployee);
+                
+                
+                
+                //if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                 //   System.out.print(currentCell.getStringCellValue() + " | ");
+               // } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                  //  System.out.print(currentCell.getNumericCellValue() + " | ");
+                //}
 
-            }
+            
             System.out.println();
-
-        }
-    } catch (FileNotFoundException exception) {
+            return employees;
+        }}    } catch (FileNotFoundException exception) {
         exception.printStackTrace();
+        return null;
     } catch (IOException exception) {
         exception.printStackTrace();
+        return null;
     }
+	return null;
 
 }
 
